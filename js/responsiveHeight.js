67e18156d3e827d1b20e6f5830760b6d3bc23dc5
Dave Gray <dave@d-gray.co.uk>
2015-06-30 21:40:30 +0100

    Home page font sizes, kerning and leading for desktop

--- js/responsiveHeight.js
+++ js/responsiveHeight.js
@@ -1,14 +1,17 @@
 function setHomeElementHeights() {
-
   var blockHeight = $('#home-image').height();
-  blockHeight = blockHeight - parseInt( $('.what-we-do').css('marginBottom') );
+  blockHeight = blockHeight - parseInt( $('.what-block').css('paddingTop') );
+  blockHeight = blockHeight - parseInt( $('.what-block').css('paddingBottom') );
+  blockHeight = blockHeight - parseInt( $('.what-block').css('marginBottom') );
+  blockHeight = blockHeight - parseInt( $('.showcase-block').css('paddingTop') );
+  blockHeight = blockHeight - parseInt( $('.showcase-block').css('paddingBottom') );
   blockHeight = blockHeight/2;
 
-  $('.what-we-do').height(blockHeight);
-  $('.what-we-do').css('line-height', blockHeight+'px');
+  $('.what-block').height(blockHeight);
+  $('.what-block').css('line-height', blockHeight+'px');
 
-  $('.showcase-project').height(blockHeight);
-  $('.showcase-project').css('line-height', blockHeight+'px');
+  $('.showcase-block').height(blockHeight);
+  $('.showcase-block').css('line-height', blockHeight+'px');
 };
 
 function setContactElementHeights() {
